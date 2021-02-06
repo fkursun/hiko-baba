9fdacb08589ba06ec2dafa34b57a4e6e933345d9
Furkan Kurşun <fkursun@poseidon9.infoshop.com.tr>
2021-02-06 16:12:47 +0300

    hikmet proje

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import shakeReducer from '../AppReducer';
 
 export default configureStore({
   reducer: {
-    counter: counterReducer,
+    shake: shakeReducer,
   },
 });
